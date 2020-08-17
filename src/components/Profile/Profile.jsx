a28234229bf1de338cb92510e614472608126a66
AntonRufus <69029538+AntonRufus@users.noreply.github.com>
2020-08-17 14:07:50 +0800

    Aug 17, 2020. #2

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,13 +3,12 @@ import profileCSS from './Profile.module.css';
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
 
-const Profile = (props) => {
+const Profile = () => {
 
     return (
         <div className={profileCSS.profile}>
             <ProfileInfo/>
             <MyPostsContainer/>
-            {/*<MyPostsContainer store={props.store}/>*/}
         </div>
     )
 }
