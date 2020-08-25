fc1e4225b68af21a94fb47fea869c7f3e3f83dd6
AntonRufus <69029538+AntonRufus@users.noreply.github.com>
2020-08-25 19:42:30 +0800

    Aug 25, 2020. #7

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -10,7 +10,7 @@ class ProfileContainer extends Component {
     componentDidMount() {
         let userId = this.props.match.params.userId;
         if (!userId) {
-            userId = 1709;
+            userId = 9457;
         }
         axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
             .then(response => {
