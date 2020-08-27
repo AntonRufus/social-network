42fe9d7622eb75f3ab79a867de2087874a23e7e1
AntonRufus <69029538+AntonRufus@users.noreply.github.com>
2020-08-27 19:13:29 +0800

    Aug 27, 2020. #3

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
 import Header from "./Header";
+import headCSS from './Header.module.css';
 import * as axios from "axios";
-import {setAuthUserData} from "../../redux/auth_reducer";
+import {setAuthUserData, toggleIsFetching} from "../../redux/auth_reducer";
 import {connect} from "react-redux";
+import Preloader from "../common/preloader/Preloader";
 
 class HeaderContainer extends Component {
 
@@ -15,19 +17,26 @@ class HeaderContainer extends Component {
                 if (response.data.resultCode === 0) {
                     let {email, id, login} = response.data.data;
                     this.props.setAuthUserData(email, id, login);
+                    this.props.toggleIsFetching(false);
                 }
                 // debugger;
             });
     }
 
     render() {
-        return <Header {...this.props}/>
+        return <>
+            <div className={headCSS.login_process}>
+                {this.props.isFetching ? <Preloader/> : null}
+            </div>
+            <div><Header {...this.props}/></div>
+        </>
     }
 }
 
 const mapStateToProps = (state) => ({
     isAuth: state.auth.isAuth,
     login: state.auth.login,
+    isFetching: state.auth.isFetching,
 });
 
-export default connect(mapStateToProps, {setAuthUserData})(HeaderContainer);
+export default connect(mapStateToProps, {setAuthUserData, toggleIsFetching})(HeaderContainer);
--- src/redux/auth_reducer.js
+++ src/redux/auth_reducer.js
@@ -1,11 +1,12 @@
 const SET_USER_DATA = 'SET_USER_DATA';
+const TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING';
 
 let initialState = {
     id: null,
     email: null,
     login: null,
-    isFetching: true,
     isAuth: false,
+    isFetching: true,
 };
 
 const authReducer = (state = initialState, action) => {
@@ -16,11 +17,19 @@ const authReducer = (state = initialState, action) => {
                 ...action.data,
                 isAuth: true,
             }
+
+        case TOGGLE_IS_FETCHING:
+            return {
+                ...state,
+                isFetching: action.isFetching
+            }
         default:
             return state;
     }
 };
 
 export const setAuthUserData = (email, id, login) => ({type: SET_USER_DATA, data: {email, id, login}});
+
+export const toggleIsFetching = (isFetching) => ({type: TOGGLE_IS_FETCHING, isFetching});
 
 export default authReducer;
