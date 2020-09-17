6cce96d97e999dc72f70b3d994893ac21db96663
AntonRufus <69029538+AntonRufus@users.noreply.github.com>
2020-09-17 13:19:09 +0800

    Sep 17, 2020. #1 useEffect, hook ProfileStatusWithHooks.jsx

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import profInfoCSS from "./ProfileInfo.module.css";
 
 const ProfileStatusWithHooks = (props) => {
@@ -6,18 +6,22 @@ const ProfileStatusWithHooks = (props) => {
     let [editMode, setEditMode] = useState(false);
     let [status, setStatus] = useState(props.status);
 
+    useEffect(() => {
+        setStatus(props.status);
+    }, [props.status]);
+
     const activateEditMode = () => {
         setEditMode(true);
-    }
+    };
 
     const deactivateEditMode = () => {
         setEditMode(false);
         props.updateStatus(status);
-    }
+    };
 
     const onStatusChange = (event) => {
         setStatus(event.currentTarget.value);
-    }
+    };
 
     return <>
         <div className={profInfoCSS.status}>My status:</div>
