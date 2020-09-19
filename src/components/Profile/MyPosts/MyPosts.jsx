0a8f45952ce4f54bcfb32a31c133406018f95a4a
AntonRufus <69029538+AntonRufus@users.noreply.github.com>
2020-09-19 12:57:31 +0800

    Sep 19, 2020. #2 memo, ShouldComponentUpdate, PureComponent

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,36 +3,35 @@ import postsCSS from './MyPosts.module.css';
 import Post from "./Post/MyPost";
 import AddPostFormRedux from "./PostForm";
 
-class MyPosts extends React.Component {
-    render() {
-        let postsElements = this.props.posts
-            .map(post =>
-                <Post message={post.message}
-                      likesCount={post.likesCount}
-                      dislikesCount={post.dislikesCount}
-                      url={post.url}
-                      key={post.id}
-                />
-            );
+const MyPosts = React.memo((props) => {
+    console.log('render in process')
+    let postsElements = props.posts
+        .map(post =>
+            <Post message={post.message}
+                  likesCount={post.likesCount}
+                  dislikesCount={post.dislikesCount}
+                  url={post.url}
+                  key={post.id}
+            />
+        );
 
-        let addNewPost = (values) => {
-            this.props.addPost(values.newPostText);
-        }
-
-        return (
-            <div className={postsCSS.my_posts_main}>
-                <div className={postsCSS.my_posts_label}>
-                    <h3>MY POSTS</h3>
-                </div>
-                <div className={postsCSS.textarea}>
-                    <AddPostFormRedux onSubmit={addNewPost}/>
-                </div>
-                <div className={postsCSS.posts_block}>
-                    {postsElements}
-                </div>
-            </div>
-        )
+    let addNewPost = (values) => {
+        props.addPost(values.newPostText);
     }
-}
+
+    return (
+        <div className={postsCSS.my_posts_main}>
+            <div className={postsCSS.my_posts_label}>
+                <h3>MY POSTS</h3>
+            </div>
+            <div className={postsCSS.textarea}>
+                <AddPostFormRedux onSubmit={addNewPost}/>
+            </div>
+            <div className={postsCSS.posts_block}>
+                {postsElements}
+            </div>
+        </div>
+    )
+});
 
 export default MyPosts;
