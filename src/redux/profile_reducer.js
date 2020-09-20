094749a8815433909527dd514063a1fdf89dfd97
AntonRufus <69029538+AntonRufus@users.noreply.github.com>
2020-09-20 13:15:03 +0800

    Sep 20, 2020. #1 TDD, test for profile_reducer.js

--- src/redux/profile_reducer.js
+++ src/redux/profile_reducer.js
@@ -4,6 +4,7 @@ import {profileAPI, usersAPI} from "../api/api";
 const ADD_POST = 'ADD-POST';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
 const SET_STATUS = 'SET_STATUS';
+const DELETE_POST = 'DELETE_POST';
 
 let initialState = {
     posts: [
@@ -59,6 +60,8 @@ const profileReducer = (state = initialState, action) => {
                 posts: [...state.posts, newPost],
             }
         }
+        case DELETE_POST:
+            return {...state, }
         case SET_USER_PROFILE:
             return {
                 ...state,
@@ -75,6 +78,8 @@ const profileReducer = (state = initialState, action) => {
 };
 
 export const addPost = (newPostText) => ({type: ADD_POST, newPostText});
+
+export const deletePost = (postId) => ({type: DELETE_POST, postId});
 
 export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile});
 
---
+++ src/redux/profile_reducer.test.js
@@ -0,0 +1,72 @@
+import profileReducer, {addPost, deletePost} from "./profile_reducer";
+import React from 'react';
+
+let state = {
+    posts: [
+        {
+            id: 1,
+            message: 'Hello!',
+            likesCount: 1005,
+            dislikesCount: 105,
+            url: 'https://pixy.org/src/480/thumbs190/4800648.jpg'
+        },
+        {
+            id: 2,
+            message: 'How"re you?',
+            likesCount: 100500,
+            dislikesCount: 1050,
+            url: 'https://cache.desktopnexus.com/thumbseg/1753/1753481-bigthumbnail.jpg'
+        },
+        {
+            id: 3,
+            message: 'How"s your life?',
+            likesCount: 1050,
+            dislikesCount: 150,
+            url: 'https://hdwallsbox.com/wallpapers/m/12/nature-flowers-macro-m11645.jpg'
+        },
+        {
+            id: 4,
+            message: 'How"s your day?',
+            likesCount: 150,
+            dislikesCount: 15,
+            url: 'https://avatars.mds.yandex.net/get-pdb/1590269/e908d30d-87d8-47e8-9c56-c5d92fd9ff79/s375'
+        },
+    ],
+    likesCountDefault: 10,
+    dislikesCountDefault: 1,
+    urlDefault: 'https://www.akibanation.com/wp-content/uploads/2016/07/Kon.BLEACH.full_.170410-150x150.jpg',
+    // urlDefault: antPortrait,
+    profile: null,
+    status: 'default status',
+};
+
+test('length post should be incremented', () => {
+    // 1. test data
+    let action = addPost('My first TDD')
+    // 2. action
+    let newState = profileReducer(state, action);
+    // 3. expectation
+    expect(newState.posts.length).toBe(5);
+});
+
+test('message of new post should be correct', () => {
+    // 1. test data
+    let action = addPost('My first TDD');
+    // 2. action
+    let newState = profileReducer(state, action);
+    // 3. expectation
+    expect(newState.posts[4].message).toBe('My first TDD');
+});
+
+test('after deleting length should be decrement', () => {
+    // 1. test data
+    let action = deletePost(1);
+    // 2. action
+    let newState = profileReducer(state, action);
+    // 3. expectation
+    expect(newState.posts.length).toBe(3);
+});
+
+
+
+
