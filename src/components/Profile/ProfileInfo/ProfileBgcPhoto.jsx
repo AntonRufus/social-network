49e7f0c5dbb18af50362dce81d0f134b0656b341
AntonRufus <69029538+AntonRufus@users.noreply.github.com>
2020-10-20 14:34:39 +0800

    Oct 20, 2020. #4

--- src/components/Profile/ProfileInfo/ProfileBgcPhoto.jsx
+++ src/components/Profile/ProfileInfo/ProfileBgcPhoto.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import profInfoCSS from './ProfileInfo.module.css';
 
 const ProfileBgcPhoto = () => {
-    return (
+    return <>
         <div className={profInfoCSS.profile_block}>
             <img className={profInfoCSS.bgc_photo}
                  src='/social-network/pic/bgc200.jpg'
                  alt='backgroundPic'/>
         </div>
-    )
+    </>
 }
 
 export default ProfileBgcPhoto;
